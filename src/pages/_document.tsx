5a2ad5d7a97d23aa331495f63df2aa89ea6af714
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-05-23 12:07:43 +0200

    fix(emotion): extract critical css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,18 @@ type Props = {
 } & DocumentProps;
 
 export default class MyDocument extends Document<Props> {
-  static async getInitialProps({ renderPage }: DocumentContext) {
-    const page = await renderPage();
-    const styles = extractCritical(page.html);
-    return { ...page, ...styles };
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style data-emotion-css={styles.ids.join(' ')} dangerouslySetInnerHTML={{ __html: styles.css }} />
+        </>
+      ),
+    };
   }
 
   render() {
